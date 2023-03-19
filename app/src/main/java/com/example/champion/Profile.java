09975d8f4f80cfccb06c40f87fe4ba331bd44533
aleksynders <aleksynders@yandex.ru>
2023-03-19 15:25:56 +0300

    i'm all :(

--- app/src/main/java/com/example/champion/Profile.java
+++ app/src/main/java/com/example/champion/Profile.java
@@ -2,13 +2,55 @@ package com.example.champion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.champion.Adapter.AdapterMaskMeditation;
 
 public class Profile extends AppCompatActivity {
+
+    TextView tvName;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvName = findViewById(R.id.tvName);
+        tvName.setText(Onboarding.nickName);
+        image = findViewById(R.id.avatar);
+        new AdapterMaskMeditation.DownloadImageTask((ImageView) image)
+                .execute(Onboarding.avatar);
     }
+
+    public  void Menu(View view) {
+        startActivity(new Intent(this, Menu.class));
+    }
+
+    public void Login(View view) {
+        SharedPreferences prefs = getSharedPreferences(
+                "Date", Context.MODE_PRIVATE);
+        prefs.edit().putString("Avatar", "").apply();
+        prefs.edit().putString("NickName", "").apply();
+
+        startActivity(new Intent(this, Login.class));
+    }
+
+    public  void Home(View view)
+    {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void Listen(View view)
+    {
+        startActivity(new Intent(this, Listen.class));
+    }
+
+
 }
